f16c6438322278090378875811ff69ea11d0cbd5
wuxiaozhou <wubaixing1379@163.com>
2017-06-08 18:07:19 +0800

    add comment

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -10,8 +10,10 @@ import { Product, Comment, ProductService } from '../shared/product.service';
 export class ProductDetailComponent implements OnInit {
 
   public product: Product;
-
   public comments: Array<Comment>;
+  public newRating: number = 5;
+  public newComment: string = "";
+  public isCommentHidden: true;
 
   constructor(
     private routeInfo:ActivatedRoute,
@@ -22,6 +24,18 @@ export class ProductDetailComponent implements OnInit {
     let productId:number = this.routeInfo.snapshot.params["productId"];
     this.product = this.productService.getProduct(productId);
     this.comments = this.productService.getCommentsForProductId(productId);
+  }
+
+  addComment() {
+    let comment = new Comment(0, this.product.id, new Date().toISOString(), "someone", this.newRating, this.newComment);
+    this.comments.unshift(comment);
+
+    let sum = this.comments.reduce((sum, comment) => sum + comment.rating, 0);
+    this.product.rating = sum / this.comments.length;
+
+    this.newComment = null;
+    this.newRating = 5;
+    this.isCommentHidden = true;
   }
 
 }
